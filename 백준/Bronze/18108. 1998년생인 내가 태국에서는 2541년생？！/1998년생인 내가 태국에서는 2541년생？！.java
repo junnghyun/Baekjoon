56c9b307361732b98a163d474471f74cf610f334
안정현 <118995172+junnghyun@users.noreply.github.com>
2024-09-12 10:41:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 168 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java. util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int teyear= sc.nextInt();
+        int syear = teyear-543;
+        System.out.println(syear);
+
+        sc.close();
+    }
+}
