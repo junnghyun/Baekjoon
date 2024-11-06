fa198672f2c4c6e263bda6f41c371ffbcec13833
안정현 <118995172+junnghyun@users.noreply.github.com>
2024-11-06 22:53:09 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 196 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			
+			System.out.println(str);
+		}
+		sc.close();
+
+    }
+
+}
